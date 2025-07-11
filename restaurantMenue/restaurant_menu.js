ca3ac280c1b42e8a6945fcef429cf84b4863318d
Faiqarif779 <mfaiqarif005@gmail.com>
2025-07-11 13:40:45 -0400

    Task of Restaurant menu

--- restaurantMenue/restaurant_menu.js
+++ restaurantMenue/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict- $22.99', 'Oatmeal- $21.99', 'Frittata- $15'];
+const mainCourseMenu = ['Steak- $21', 'Pasta- $40', 'Burger- $35', 'Salmon- $27'];
+const dessertMenu = ['Cake- $50', 'Ice Cream- $15', 'Pudding-$23', 'Fruit Salad- $65'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
