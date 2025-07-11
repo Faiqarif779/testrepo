3a929ac87b2e609d3e389e455165b330869ec79f
Faiqarif779 <mfaiqarif005@gmail.com>
2025-07-11 09:56:51 -0400

    Followers reset button

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -16,5 +16,15 @@ function checkCountvalue(){
         alert("Your Instagram flowers gained 10 followers! Congratulations!");
     }else if(count === 20){
         alert("Your Instagram flowers gained 20 followers! Keep it up!");
+    }else if(count === 0 ){
+        alert("The Followers count has been reset.");
     }
+}
+function resetCount(){
+    count = 0;
+
+    displayCount();
+
+    checkCountvalue();
+
 }
