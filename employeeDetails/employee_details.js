72c8d62ff1360ab02e3b77dede07f5d2b8b2a08d
Faiqarif779 <mfaiqarif005@gmail.com>
2025-07-11 14:54:30 -0400

    Employee Specialization

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,14 +1,15 @@
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'JavaScript' },
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Python' },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Java' },
       //... More employee records can be added here
     ];
 
  // Function to display all employees
+ function displayEmployees(){
 const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name} - ${employee.age} - ${employee.department} - ${employee.salary}</p>`).join('');
         document.getElementById('employeesDetails').innerHTML = totalEmployees;
-
+ }
         function calculateTotalSalaries() {
       const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
       alert(`Total Salaries: ${totalSalaries}`);
@@ -30,4 +31,8 @@ function findEmployeeById(employeeId) {
        }
    }
 
-   
+function specializtionJavaScript(){
+    const spjs = employees.filter(employee => employee.specialization === 'JavaScript');
+    const spjsdisplay = spjs.map((employee,index)=> `<p>${employee.id}: ${employee.name} - ${employee.age} - ${employee.department} - ${employee.salary}</p>`).join('');
+    document.getElementById('employeesDetails').innerHTML = spjsdisplay;
+}
